f9bb78f72f820b61907d792ef4a99b2ac5a9057c
Gaurav Kamble <gauravkamble193@outlook.com>
2026-02-01 16:28:32 +0530

    Refactor MongoDB connection to use MONGO_URI
    Updated MongoDB connection to use environment variable for URI.

--- init.js
+++ init.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect("mongodb+srv://Gauravkamble:0112@blogs.jcxrkww.mongodb.net/?appName=blogs")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+  .catch(err => console.error("Mongo error:", err.message));
